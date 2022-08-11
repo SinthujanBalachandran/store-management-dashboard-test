41a8b298a0968aec106c552d8db875770b2e1318
SinthujanBalachandran <54698571+SinthujanBalachandran@users.noreply.github.com>
2022-08-12 00:25:30 +0530

    second commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,8 +13,12 @@ sun.onclick = function() {
 
 let menu = document.querySelector(".menu");
 let sidebar = document.querySelector(".sidebar");
+let mainContainer = document.querySelector(".main--container")
 
 menu.onclick = function(){
     sidebar.classList.toggle("activemenu");
    
+}
+mainContainer.onclick = function(){
+    sidebar.classList.remove("activemenu");
 }
